f245d5d154f8eb9b501d6475812595e7770b60a5
uHirsch <66682042+Swr96@users.noreply.github.com>
2021-12-04 17:35:00 +0100

    added more mockups for dashboard and examView

--- nEXam/app/src/main/java/com/example/nexam/MainActivity.kt
+++ nEXam/app/src/main/java/com/example/nexam/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.nexam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import android.widget.ArrayAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.exam_view)
+
+        val examListView = findViewById<ListView>(R.id.content_list)
+        examListView.adapter = ArrayAdapter(this,android.R.layout.simple_expandable_list_item_1,resources.getStringArray(R.array.test_content))
+
+
     }
 }
