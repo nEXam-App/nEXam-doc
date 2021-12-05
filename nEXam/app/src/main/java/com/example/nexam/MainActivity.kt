87f60abf1a2d1f19443694918c6e238401118ed3
uHirsch <66682042+Swr96@users.noreply.github.com>
2021-12-05 10:10:05 +0100

    added mockups examSucces and examError

--- nEXam/app/src/main/java/com/example/nexam/MainActivity.kt
+++ nEXam/app/src/main/java/com/example/nexam/MainActivity.kt
@@ -8,10 +8,10 @@ import android.widget.ArrayAdapter
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.exam_view)
+        setContentView(R.layout.exam_error)
 
-        val examListView = findViewById<ListView>(R.id.content_list)
-        examListView.adapter = ArrayAdapter(this,android.R.layout.simple_expandable_list_item_1,resources.getStringArray(R.array.test_content))
+        //val examListView = findViewById<ListView>(R.id.content_list)
+        //examListView.adapter = ArrayAdapter(this,android.R.layout.simple_expandable_list_item_1,resources.getStringArray(R.array.test_content))
 
 
     }
