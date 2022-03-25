0142d778e913afa87b1b2ecb0fed240789e5f899
Franziska1402 <franziskaommer@gmail.com>
2022-03-25 23:40:01 +0100

    NEX-113 removed hardcoded size of listview

--- app/src/main/java/com/example/nexam/MainActivity.kt
+++ app/src/main/java/com/example/nexam/MainActivity.kt
@@ -10,14 +10,16 @@ import android.widget.Button
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dashboard)
         loadView()
 
     }
 
-    fun loadView(){
+    private fun loadView() {
         registerButton(R.id.createExamButton, R.layout.create_exam)
         registerButton(R.id.startTimer, R.layout.exam_view)
         registerButton(R.id.back, R.layout.dashboard)
@@ -29,29 +31,29 @@ class MainActivity : AppCompatActivity() {
         fillList(R.id.content_list, R.array.test_content)
     }
 
-    fun registerButton(button:Int, view:Int){
-        val button:Button? = findViewById(button)
-        if(button==null) return
+    private fun registerButton(button: Int, view: Int) {
+        val button: Button? = findViewById(button)
+        if (button == null) return
         button.setOnClickListener(View.OnClickListener {
             setContentView(view)
             loadView()
         })
     }
 
-    fun fillList(list:Int, array:Int){
-        val examListView:ListView? = findViewById(list)
-        if(examListView == null) return
-        examListView.setAdapter(ArrayAdapter(this,android.R.layout.simple_expandable_list_item_1,resources.getStringArray(array)))
-        examListView.onItemClickListener = object :AdapterView.OnItemClickListener{
-            override fun onItemClick(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
+    private fun fillList(list: Int, array: Int) {
+        val examListView: ListView? = findViewById(list)
+        if (examListView == null) return
+        examListView.adapter =
+            ArrayAdapter(
+                this,
+                android.R.layout.simple_expandable_list_item_1,
+                resources.getStringArray(array)
+            )
+
+        examListView.onItemClickListener =
+            AdapterView.OnItemClickListener { parent, view, position, id ->
                 setContentView(R.layout.exam_view)
                 loadView()
             }
-        }
     }
 }
